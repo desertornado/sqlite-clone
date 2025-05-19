50dddee5d205d672673ef88f97811f33b381c263
desertornado <andrea.menichelli.sw@gmail.com>
2025-05-19 12:53:06 +0200

    enum input type defined

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,17 +1,30 @@
 #include <fstream>
 #include <iostream>
 
+enum InputType {
+    EXIT,
+    HELP,
+    OTHER
+};
 
 int main() {
-
     std::cout << "Hello and welcome to my FOSS c++ sqlite clone!\n";
     std::string input;
     while (true) {
-        std::cout << "Please enter a command: (exit)\n";
+        std::cout << "Please enter a command: (exit, help)\n";
         std::getline(std::cin, input);
 
-        if (input == "exit") {
-            return EXIT_SUCCESS;
+        switch (input) {
+            case EXIT:
+                return EXIT_SUCCESS;
+
+            case HELP: {
+                std::cout << "Syntax TBD";
+                break;
+            }
+
+            case OTHER:
+                break;
         }
     }
 
