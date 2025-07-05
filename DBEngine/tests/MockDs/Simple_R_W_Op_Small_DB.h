387755bb41093582ef6d444f79ae193f2e4d8ada
desertornado <andrea.menichelli.sw@gmail.com>
2025-07-05 12:33:34 +0200

    moved struct

--- DBEngine/tests/MockDs/Simple_R_W_Op_Small_DB.h
+++ DBEngine/tests/MockDs/Simple_R_W_Op_Small_DB.h
@@ -11,14 +11,16 @@
 class Simple_R_W_Op_Small_DB {
 
     typedef struct {
+        std::string column_name;
+        std::string column_value;
+    } mock_column;
+
+    typedef struct {
         std::string table_name;
         std::vector<mock_column> columns;
     } mock_table;
 
-    typedef struct {
-        std::string column_name;
-        std::string column_value;
-    } mock_column;
+
 
 public:
     /**
